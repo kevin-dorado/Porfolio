c2b70eb6638b845eeb36651b0616f9e307086f38
kevin-dorado <kevindorado2811@gmail.com>
2023-09-11 10:43:47 -0300

    navbar finished

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link,useLocation } from "react-router-dom"
 
 const Nav = () => {
+    const location = useLocation();
     return(
         <>
             <nav className="lg:sticky mb-16 mt-4">
                 <div className=" flex">
-                    <Link to="/"  className="text-neutral-100 mr-2  border-b border-neutral-400 border-opacity-50" href="#"> <span>About Me</span>    </Link>
-                    <Link to="/porfolio" className="text-neutral-400 mr-2  hover:text-neutral-100" href="#"> <span>Portfolio</span>    </Link>
-                    <Link to="/contact-me" className="text-neutral-400    hover:text-neutral-100" href="#"> <span>Contact Me</span>    </Link>
+                    <Link to="/"  className={`mr-2 ${location.pathname === "/" ? "text-neutral-100 border-b border-neutral-400 border-opacity-50 " : "text-neutral-400 hover:text-neutral-100"}`} > <span>About Me</span></Link>
+                    <Link to="/porfolio" className={`mr-2 ${location.pathname === "/porfolio" ? "text-neutral-100 border-b border-neutral-400 border-opacity-50 " : "text-neutral-400 hover:text-neutral-100"}`} > <span>Portfolio</span></Link>
+                    <Link to="/contact-me"  className={`mr-2 ${location.pathname === "/contact-me" ? "text-neutral-100 border-b border-neutral-400 border-opacity-50 " : "text-neutral-400 hover:text-neutral-100"}`} > <span>Contact Me</span></Link>
                 </div>
-            
             </nav>
         </>
     )
