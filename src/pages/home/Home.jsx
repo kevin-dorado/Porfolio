c7ca2560d1d6b3b3623875e411f473293499a489
kevin-dorado <kevindorado2811@gmail.com>
2023-09-11 20:06:18 -0300

    project started

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -28,8 +28,13 @@ return(
             mb-4 font-semibold mt-6" >Projects</h2>
         <p className="text-neutral-300 ">In this section you will find some of the projects I have done. Here you can see examples of my work, the technologies I use and the objectives I completed, I hope you like them.</p>
 
-        <div className="mt-5 bg-slate-500 rounded-xl">
-            <img src="/CaloriasPorfolio.png" alt="" className="rounded-lg" />
+        <div className="mt-5 bg-neutral-800 border-2  border-neutral-700 rounded-xl p-3">
+            <div className="">
+                <img src="/CalPorfolio1.png" alt="" className="rounded-lg " />   
+            </div>
+            <h3  className="text-neutral-100 text-xl 
+            mb-4 font-semibold mt-6" >Calories Calculator</h3>
+            <p className="text-neutral-300 ">This program allows you to calculate your daily calorie needs, calories to lose fat and calories to gain muscle according to your height, weight, age, gender and activity level. It is made with HTML, JavaScript and Tailwind.</p>
         </div>
     </section>
 </>
