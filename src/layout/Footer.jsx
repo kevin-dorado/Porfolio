a192081faac8813066cdf39d9c5d9dffafed73ea
kevin-dorado <kevindorado2811@gmail.com>
2023-09-12 12:05:49 -0300

    footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,27 +1,30 @@
 import {BsLinkedin,BsGithub} from "react-icons/bs";
-
+import {SiGmail} from "react-icons/si";
 export const Footer = () =>{
 return(
     <>
-        <footer className=" mt-12">    
-            <div className="flex justify-center">   
-                <img src="/klogofondo.png" alt="" className="w-16" />  
-            </div>
-            <div className="flex justify-center text-neutral-300 mt-5">
-                <div className="mr-2">
-                    <a href="">
-                        <BsLinkedin/>
-                    </a>
-                </div> 
-                <div>
-                    <a href="#">
-                        <BsGithub/>    
-                    </a>  
+        <footer className=" my-10">    
+            <div className="flex justify-between">
+                <div className="flex">   
+                    <img src="/klogofondo.png" alt="" className="w-14" />  
                 </div>
-
-            </div>
-            <div className="text-zinc-600">
-                <p>@Kevin Dorado</p>
+                <div className="flex justify-end text-neutral-300 mt-5">
+                    <div className="mr-5">
+                        <a href="https://www.linkedin.com/in/kevin-dorado-64677a25b/" target="blank">
+                            <BsLinkedin/>
+                        </a>
+                    </div> 
+                    <div className="mr-5">
+                        <a href="https://github.com/kevin-dorado"  target="blank">
+                            <BsGithub/>    
+                        </a>  
+                    </div>
+                    <div className="mr-2">
+                        <a href="mailto:kevindorado2811@gmail.com?subject=Correo desde mi sitio web"  target="blank">
+                            <SiGmail/>    
+                        </a>  
+                    </div>
+                </div>
             </div>
         </footer>
     </>
