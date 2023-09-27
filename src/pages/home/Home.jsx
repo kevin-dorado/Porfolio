aafcc4ba8ff7c18a77d75fb58f10e5e5f0fb91bd
kevin-dorado <kevindorado2811@gmail.com>
2023-09-27 18:08:35 -0300

    projects fiinshed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,14 +7,14 @@ const Home= () =>{
 
 return(
 <>
-    <section >
+    <section className="mt-16">
         <h1  className="text-neutral-100 text-2xl
-        mb-10 font-semibold" > Hi I'm Kevin 👋</h1>
+        mb-7 font-semibold" > Hi I'm Kevin 👋</h1>
         <p className="text-neutral-300 ">I am a 20 year old junior web-developer, who was graduated from the Instituto Tecnológico Superior Arias-Balparda and then, at the Jovenes a Programar web-development bootcamp. I love exploring new technologies and expanding my knowledge in the field of web development.</p>
     </section>
-    <section>
+    <section className="mt-16">
         <h2  className="text-neutral-100 text-xl 
-        mb-4 font-semibold mt-7" >Skills</h2>
+        mb-4 font-semibold " >Skills</h2>
         <div className="flex">
             <ToolTip icon={<DiJavascript1 size={25} />} tec={"Javascript"} tc={"text-yellow-300"} href={"https://developer.mozilla.org/en-US/docs/Web/JavaScript"}/>
             <ToolTip icon={<DiReact size={25} />} tec={"React"} tc={"text-cyan-300"} href={"https://es.react.dev/"}/>
@@ -24,15 +24,14 @@ return(
             <ToolTip  icon={<DiVisualstudio size={25} />} tec={"Visual Basic"} tc={"text-violet-600"} href={"https://learn.microsoft.com/es-es/dotnet/visual-basic/"}/>
         </div>
     </section>
-    <section>
+    <section className="mt-16">
         <h2  className="text-neutral-100 text-xl 
                     mb-4 font-semibold mt-6" >Projects</h2>
         <p className="text-neutral-300 ">In this section you will find some of the projects I have done. Here you can see examples of my work, the technologies I use and the objectives I completed, I hope you like them.</p>
-        <CardProject img={"/CalPorfolio1.png"} text={"This program allows you to calculate your daily calorie needs, calories to lose fat and calories to gain muscle according to your height, weight, age, gender and activity level. It is made with HTML, JavaScript and Tailwind."} title={"Calories Calculator"}/>
-        <CardProject/>
-        <CardProject/>
-        <CardProject/>
-        <CardProject/>
+        <CardProject img={"/CalPorfolio1.png"} text={"This program allows you to calculate your daily calorie needs, calories to lose fat and calories to gain muscle according to your height, weight, age, gender and activity level. It is made with HTML, JavaScript and Tailwind."} title={"Calories Calculator"} page={"https://calculadora-de-calorias-seven.vercel.app/"}/>
+        <CardProject img={"/TwFollowPorfolio.png"} text={"This project is an imitation of the who to follow template from Twitter, made with React and Tailwind. It uses the avatar.io API to generate images of the suggested users. The goal is to practice the use of functional components, hooks and styles with Tailwind."} title={"Twitter Follow"} page={"https://twitter-follow.vercel.app/"}/>
+        <CardProject img={"/e.comerce1.png"} text={"This project is an E-commerce page created in Jóvenes A Programar. It was developed using the Bootstrap framework and JavaScript. The page has a static database and utilizes localstorage. The website includes a shopping cart, user profiles, and different product categories."} title={"e.mercado"} page={"https://keviin999-github-1grrqca04-kevindorado2811-gmailcom.vercel.app"}/>
+        <CardProject img={"/andreabeautyink.png"} text={" Website created with WIX at the request of a Micro-Blading company, located in the U.S.A. The website has a services section, a gallery of previous works, information about the owner and a FAQ section."} title={"Andrea Beauty Ink."} page={"https://www.andreabeautyink.com/"}/>
     </section>
 </>
 )
--- src/utilities/CardProject.jsx
+++ src/utilities/CardProject.jsx
@@ -1,4 +1,6 @@
-export const CardProject = ({img,title,text}) => {
+import { CornerDownRight } from 'lucide-react';
+
+export const CardProject = ({img,title,text,page}) => {
     return(
         <>
                 <div className="mt-5 bg-neutral-800 border-2  border-neutral-700 rounded-xl p-3">
@@ -8,6 +10,16 @@ export const CardProject = ({img,title,text}) => {
                     <h3  className="text-neutral-100 text-xl 
                     mb-4 font-semibold mt-6" >{title} </h3>
                     <p className="text-neutral-300 ">{text}</p>
+
+                    <a href={page} target="blank" className='inline-block pt-2 ' >
+                        <div className='flex text-neutral-500  hover:text-neutral-100'>
+                            <span className='py-1 mr-1'>See the page </span>
+                            <div className='py-1 pt-2'>
+                                <CornerDownRight size={18}/>
+                            </div>
+
+                        </div>
+                    </a>
                 </div>   
         </>
     )
