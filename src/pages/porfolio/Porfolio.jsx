8502f4af3339f655063edbf19214df64f06d610f
kevin-dorado <kevindorado2811@gmail.com>
2023-09-27 19:11:18 -0300

    porfolio started

--- src/pages/porfolio/Porfolio.jsx
+++ src/pages/porfolio/Porfolio.jsx
@@ -1,9 +1,13 @@
 import { Result } from "postcss"
-
 export const Porfolio = () =>{
     return(
         <>
-            <h1 className=" text-neutral-100">Porfolio</h1>
+            <section className="mt-16">
+                <h1  className="text-neutral-100 text-2xl
+                mb-7 font-semibold" > Porfolio 📖</h1>
+                <p className="text-neutral-300 ">In this section, you will find my portfolio, where you can see my diplomas, titles, and certifications that I have obtained throughout my career. Each of these documents is a testament to my dedication, learning, and growth in various areas of specialization.</p>
+            </section>
+
         </>
     )
 }
