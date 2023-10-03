b3b4de476839c90425a5ec1e0543658ae9354cd5
kevin-dorado <kevindorado2811@gmail.com>
2023-10-03 18:14:10 -0300

    contact me design

--- src/pages/contact me/ContactMe.jsx
+++ src/pages/contact me/ContactMe.jsx
@@ -4,10 +4,11 @@ export const ContactMe = () =>{
 
             <section className="mt-16">
                 <h1  className="text-neutral-100 text-2xl
-                mb-7 font-semibold" > Contact Me </h1>
+                mb-7 font-semibold" > Contact Me 📩 </h1>
                 <p className="text-neutral-300">This space is designed so you can communicate with me in an easily and directly. If you are interested in my work as a web designer, or if you have any questions or proposals, don't hesitate to leave me a message !</p>
+                <p  className="text-neutral-300 mt-3">find me on <a href="https://www.linkedin.com/in/kevin-dorado-64677a25b/" target="blank" ><strong>LinkedIn</strong></a>, or email me at <strong>kevindorado2811@gmail.com</strong></p>
             </section>  
-            <section className="mt-16">   
+            <section className="mt-5">   
                 <div className="md:w-full text-lg text-neutral-100 bg-neutral-800 border-2  border-neutral-700 rounded-xl p-3 ">
                     <form className="m-2" action="">
                         <div className="flex justify-between">
@@ -25,7 +26,7 @@ export const ContactMe = () =>{
                             <textarea  className="resize-none w-full pb-5 rounded-md  px-2 text-neutral-800 " type="text" />
                         </div>
                         
-                        <div className="flex justify-end mt-6">
+                        <div className="flex justify-end mt-4">
                             <button className=" p-1 px-8 text-neutral-100 bg-neutral-800 hover:bg-[#111010] border-2  border-neutral-700 rounded-xl ">
                             Send
                             </button>
