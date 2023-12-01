4483c7ea976b71ba7471ddfe29c08cbbe24fe270
kevin-dorado <kevindorado2811@gmail.com>
2023-12-01 17:15:55 -0300

    redisign home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -25,13 +25,12 @@ return(
         </div>
     </section>
     <section className="mt-16">
-                <h1  className="text-neutral-100 text-2xl
-                mb-7 font-semibold" > CV </h1>
+                <h1  className="text-neutral-100 text-xl 
+        mb-4 font-semibold " > CV </h1>
                 <p className="text-neutral-300 ">In this section, you will find my</p>
     </section>
     <section className="mt-16">
                 <Certifications title={"Currículum Vitae"} width={"640"} height={"950"} src={"https://drive.google.com/file/d/1pZdw9oxgaUcEdv1NKo-eXFBGgYL_r1fO/preview"}/>
-                <Certifications title={"Full Stack Software Developer Assessment"} width={"640"} height={"550"} src={"https://drive.google.com/file/d/1tNn-3NCwEtmhCbJ__WHeJOAxwkrWCE3h/preview"}/>
     </section> 
 </>
 )
